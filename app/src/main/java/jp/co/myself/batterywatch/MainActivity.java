2c9acdbe040b399337ace16643c3906472a8dbe6
xda-yoshioka <xda-yoshioka@kddilabs.jp>
2016-01-27 15:17:51 +0900

    (新規作成)

---
+++ app/src/main/java/jp/co/myself/batterywatch/MainActivity.java
@@ -0,0 +1,96 @@
+package jp.co.myself.batterywatch;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.Calendar;
+
+public class MainActivity extends Activity {
+
+    private MyBroadcastReceiver mReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mReceiver = new MyBroadcastReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver(mReceiver, filter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public class MyBroadcastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            if (intent.getAction().equals(Intent.ACTION_BATTERY_CHANGED)) {
+                // 電池残量の最大値を取得します。
+                int scale = intent.getIntExtra("scale", 0);
+                // 電池残量を取得します。
+                int level = intent.getIntExtra("level", 0);
+                int status = intent.getIntExtra("status", 0);
+                String statusString = "";
+                switch (status) {
+                    case BatteryManager.BATTERY_STATUS_UNKNOWN:
+                        statusString = "unknown";
+                        break;
+                    case BatteryManager.BATTERY_STATUS_CHARGING:
+                        statusString = "charging";
+                        break;
+                    case BatteryManager.BATTERY_STATUS_DISCHARGING:
+                        statusString = "discharging";
+                        break;
+                    case BatteryManager.BATTERY_STATUS_NOT_CHARGING:
+                        statusString = "not charging";
+                        break;
+                    case BatteryManager.BATTERY_STATUS_FULL:
+                        statusString = "full";
+                        break;
+                }
+                final Calendar calendar = Calendar.getInstance();
+                final int hour = calendar.get(Calendar.HOUR_OF_DAY);
+                final int minute = calendar.get(Calendar.MINUTE);
+                final int second = calendar.get(Calendar.SECOND);
+                Log.v("Battery Watch", "" + hour + ":" + minute + ":" + second + " " + statusString + " " + level + "/" + scale);
+            }
+        }
+    }
+
+}
